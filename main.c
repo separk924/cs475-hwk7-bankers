22ea2630b6540f3671f7ef95d1c3ed4dd9035605
Seung <separk@pugetsound.edu>
2022-04-20 22:21:29 -0400

    got file path and basic structure of the boolean statements

--- main.c
+++ main.c
@@ -2,11 +2,32 @@
 #include <stdlib.h>
 #include "vector.h"
 #include "banker.h"
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
   // TODO: attempt to open scenario file and scan data into allocated structures
-
+  FILE* file;
+  char path[] = "./cs475-hwk7-bankers/";
+  char *arg = argv[1];
+  strcat(path, arg);
+  printf("%s\n", path);
+  file = fopen(arg, "r");
+  int NRES; // number of resource types
+  int NPROC; // number of processes
+  if(argc == 1){ // if no file was inputted
+    printf("Error: No file was inputted: ./bankers <filename>\n");
+  }
+  else{
+    if(file != NULL){
+      fscanf(file, "%d", &NRES);
+      fscanf(file, "%d", &NPROC);
+      int mat = malloc((NPROC*NRES)*sizeof(int));
+      
+    }else{ // if file does not exist
+      printf("Error: File does not exist\n");
+    }
+  }
   // TODO: Run banker's safety algorithm
 
   return 0;
--- vector.c
+++ vector.c
@@ -3,3 +3,22 @@
 #include "vector.h"
 
 // TODO - Define vector and matrix operations
+void clone(){
+    // clone vectors/matrices
+}
+
+int compare(){
+    // compare 2 vectors
+}
+
+int add(){
+    // add 2 vectors/matrices
+}
+
+int sub(){
+    // subtract 2 vectors/matrices
+}
+
+int print(){
+    // print contents of vectors/matrices
+}
